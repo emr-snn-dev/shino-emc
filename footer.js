31b6575d40b9f7a8b8e81698a50eb72f19d45117
emr-snn-dev <dev.snn.emc@gmail.com>
2025-11-23 23:41:35 +0900

    Add footer with navigation links and copyright

---
+++ footer.js
@@ -0,0 +1,12 @@
+document.write(`
+<footer>
+    <ul>
+        <li><a href="index.html">トップ</a></li>
+        <li><a href="challenge.html">私たちの挑戦</a></li>
+        <li><a href="machine.html">マシン紹介</a></li>
+        <li><a href="support.html">スポンサー・ご支援</a></li>
+        <li><a href="owner.html">運営者情報</a></li>
+    </ul>
+    <p>© 2025 篠ノ井技術クラブ All Rights Reserved.</p>
+</footer>
+`);
