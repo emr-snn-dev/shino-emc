d275af0454f6344aecc655355f4bac8366e05f6e
emr-snn-dev <dev.snn.emc@gmail.com>
2025-11-23 23:40:44 +0900

    Create header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+document.write(`
+<header>
+    <nav>
+        <ul>
+            <li><a href="home.html">トップ</a></li>
+            <li><a href="challenge.html">私たちの挑戦</a></li>
+            <li><a href="machine.html">マシン紹介</a></li>
+            <li><a href="support.html">スポンサー・ご支援</a></li>
+            <li><a href="owner.html">運営者情報</a></li>
+        </ul>
+    </nav>
+</header>
+`);
