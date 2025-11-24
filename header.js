35de519660c9bf54b2938ae1e1d5f1ccec32d1a0
emr-snn-dev <dev.snn.emc@gmail.com>
2025-11-24 11:41:19 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,13 +1,10 @@
-document.write(`
-<header>
-    <nav>
-        <ul>
-            <li><a href="home.html">トップ</a></li>
-            <li><a href="challenge.html">私たちの挑戦</a></li>
-            <li><a href="machine.html">マシン紹介</a></li>
-            <li><a href="support.html">スポンサー・ご支援</a></li>
-            <li><a href="owner.html">運営者情報</a></li>
-        </ul>
-    </nav>
-</header>
-`);
+document.getElementById("site-header").innerHTML = `
+<nav class="nav">
+    <ul>
+        <li><a href="index.html">トップ</a></li>
+        <li><a href="challenge.html">私たちの挑戦</a></li>
+        <li><a href="machine.html">マシン紹介</a></li>
+        <li><a href="support.html">スポンサー・ご支援</a></li>
+    </ul>
+</nav>
+`;
