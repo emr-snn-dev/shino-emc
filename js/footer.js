9e6c01a2e68c3459940d6b915cd9ee383d041eda
emr-snn-dev <dev.snn.emc@gmail.com>
2025-11-24 11:17:28 +0900

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-    const footer = document.createElement("footer");
-    footer.innerHTML = `
-        <ul>
-            <li><a href="index.html">トップ</a></li>
-            <li><a href="challenge.html">私たちの挑戦</a></li>
-            <li><a href="machine.html">マシン紹介</a></li>
-            <li><a href="support.html">スポンサー・ご支援</a></li>
-        </ul>
-        <p>© 2025 篠ノ井技術クラブ All Rights Reserved.</p>
-    `;
-
-    document.body.appendChild(footer);
-});
+document.body.insertAdjacentHTML("beforeend", `
+<footer>
+    <p>© 2025 篠ノ井技術クラブ All Rights Reserved.</p>
+</footer>
+`);
