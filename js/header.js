7fa48d4c55e50f23d6a756adda9144d174e0cf4d
emr-snn-dev <dev.snn.emc@gmail.com>
2025-11-24 13:19:30 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,12 +1,11 @@
-document.body.insertAdjacentHTML("afterbegin", `
-<header>
-    <nav>
-        <ul>
-            <li><a href="index.html">トップ</a></li>
-            <li><a href="challenge.html">私たちの挑戦</a></li>
-            <li><a href="machine.html">マシン紹介</a></li>
-            <li><a href="support.html">スポンサー・ご支援</a></li>
-        </ul>
-    </nav>
-</header>
-`);
+document.querySelector("header").innerHTML = `
+<nav>
+    <ul>
+        <li><a href="index.html">トップ</a></li>
+        <li><a href="challenge.html">私たちの挑戦</a></li>
+        <li><a href="machine.html">マシン紹介</a></li>
+        <li><a href="support.html">スポンサー</a></li>
+        <li><a href="operator.html">運営者情報</a></li>
+    </ul>
+</nav>
+`;
